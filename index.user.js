98a897a93027c3537219f9f8743a313f66b838a6
ilkecan <ilkecan@protonmail.com>
2021-07-01 17:35:52 +0300

    Add key bindings to open travel and protein modals

--- index.user.js
+++ index.user.js
@@ -15,7 +15,7 @@
 // @require     shop.js
 // @grant       none
 // @icon        https://raw.githubusercontent.com/pokeclicker/pokeclicker/develop/src/assets/images/favicon.ico
-// @version     0.3.0
+// @version     0.4.0
 // @author      ilkecan
 // @description key bindings for various things
 // ==/UserScript==
@@ -32,8 +32,10 @@ keymage('ctrl-f', () => { toggle_modal("farm"); }, { preventDefault: true });
 keymage('ctrl-h', () => { toggle_modal("breeding"); }, { preventDefault: true });
 keymage('ctrl-i', () => { toggle_modal("showItems"); }, { preventDefault: true });
 keymage('ctrl-l', () => { toggle_modal("logBook"); }, { preventDefault: true });
+keymage('ctrl-p', () => { toggle_modal("pokemonSelector"); }, { preventDefault: true });
 keymage('ctrl-q', () => { toggle_modal("Quest"); }, { preventDefault: true });
 keymage('ctrl-s', () => { toggle_modal("shop"); }, { preventDefault: true });
+keymage('ctrl-t', () => { toggle_modal("Ship"); }, { preventDefault: true });
 keymage('ctrl-u', () => { toggle_modal("mine"); }, { preventDefault: true });
 
 function set_battle_item_multiplier(index) {
