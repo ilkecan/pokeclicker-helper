8bb3fc8764dca6f23531d69512a70d27c0c695ff
ilkecan <ilkecan@protonmail.com>
2021-07-02 17:23:28 +0300

    Increase the number of oak item slots to 5

--- oak_items.js
+++ oak_items.js
@@ -1,7 +1,18 @@
+const NUMBER_OF_OAK_ITEM_LOADOUTS = 5;
+
 function use_loadout(number) {
     App.game.oakItemLoadouts.activateLoadout(number - 1);
 }
 
-keymage('alt-1', () => { use_loadout(1); }, { preventDefault: true });
-keymage('alt-2', () => { use_loadout(2); }, { preventDefault: true });
-keymage('alt-3', () => { use_loadout(3); }, { preventDefault: true });
+const original_oak_items_from_json = OakItemLoadouts.prototype.fromJSON;
+OakItemLoadouts.prototype.fromJSON = function(json) {
+    for (let i = this.loadouts.length; i < NUMBER_OF_OAK_ITEM_LOADOUTS; i += 1) {
+        this.loadouts.push(ko.observableArray());
+    }
+
+    original_oak_items_from_json.call(this, json);
+}
+
+for (let i = 1; i <= NUMBER_OF_OAK_ITEM_LOADOUTS; i += 1) {
+    keymage(`alt-${i}`, () => { use_loadout(i); }, { preventDefault: true });
+}
