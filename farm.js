77702b0f423818f824e63a95aaa3c46a4c4029ed
ilkecan <ilkecan@protonmail.com>
2021-07-03 15:07:25 +0300

    Make the shovel non-consumable

--- farm.js
+++ farm.js
@@ -1,2 +1,8 @@
+const original_farming_shovel = Farming.prototype.shovel;
+Farming.prototype.shovel = function(index) {
+    original_farming_shovel.call(this, index);
+    GameHelper.incrementObservable(this.shovelAmt, 1);
+}
+
 keymage('alt-h', () => { App.game.farming.harvestAll(); }, { preventDefault: true });
 keymage('alt-p', () => { App.game.farming.plantAll(FarmController.selectedBerry()); }, { preventDefault: true });
