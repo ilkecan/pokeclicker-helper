fd8aa87de8b790cd6106a160f23cc88a126ec000
ilkecan <ilkecan@protonmail.com>
2021-07-05 22:30:38 +0300

    Add key bindings to use restores

--- underground.js
+++ underground.js
@@ -2,6 +2,10 @@ function select_tool(name) {
     Mine.toolSelected(Mine.Tool[name]);
 }
 
+function use_item(name) {
+    ItemList[name].use();
+}
+
 underground_modal.on('show.bs.modal', () => { add_scope('underground'); });
 underground_modal.on('hide.bs.modal', () => { remove_scope('underground'); });
 
@@ -17,3 +21,7 @@ GameController.focusedOnEditableElement = function() {
 
 keymage('underground', 'shift-1', () => { select_tool("Chisel"); }, { preventDefault: true });
 keymage('underground', 'shift-2', () => { select_tool("Hammer"); }, { preventDefault: true });
+
+keymage('underground', 'ctrl-1', () => { use_item("SmallRestore"); }, { preventDefault: true });
+keymage('underground', 'ctrl-2', () => { use_item("MediumRestore"); }, { preventDefault: true });
+keymage('underground', 'ctrl-3', () => { use_item("LargeRestore"); }, { preventDefault: true });
