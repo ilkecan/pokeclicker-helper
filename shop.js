5a8a34baa036e62cfb4933028bd603b54c7e720f
ilkecan <ilkecan@protonmail.com>
2021-07-10 14:51:08 +0300

    Do not reset the amount of items on the shop modal

--- shop.js
+++ shop.js
@@ -17,5 +17,7 @@ function get_shop() {
 shop_modal.on('show.bs.modal', () => { add_scope('shop'); });
 shop_modal.on('hide.bs.modal', () => { remove_scope('shop'); });
 
+ShopHandler.resetAmount = function() {};
+
 keymage('shop', 'shift-1', () => { ShopHandler.multiplyAmount(10); }, { preventDefault: true });
 keymage('shop', 'shift-2', () => { ShopHandler.multiplyAmount(0.1); }, { preventDefault: true });
