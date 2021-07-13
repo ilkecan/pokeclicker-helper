ee77dc408ca1c14274732403fd257e39465e040e
ilkecan <ilkecan@protonmail.com>
2021-07-13 15:35:44 +0300

    Add profit column to the underground deals tab

--- underground.js
+++ underground.js
@@ -19,6 +19,59 @@ GameController.focusedOnEditableElement = function() {
     return original_focused_on_editable_element.call(this);
 }
 
+DailyDeal.prototype.get_profit = function() {
+    let item1_value = this.item1.value;
+    let item2_value = this.item2.value;
+    let item1_amount = this.amount1;
+    let item2_amount = this.amount2;
+
+    if (this.item1.valueType !== "Diamond") {
+        item1_value = 0;
+
+        if (this.item1.isStone()) {
+            item1_amount = 0;
+        }
+    }
+
+    if (this.item2.valueType !== "Diamond") {
+        item2_value = 0;
+
+        if (this.item2.isStone()) {
+            item2_amount = 0;
+        }
+    }
+
+    if (item1_value === 0 && item2_value === 0) {
+        const profit = item2_amount - item1_amount;
+
+        if (profit === 0) {
+            return "---";
+        } else if (profit > 0) {
+            return `+${profit.toString().padEnd(2)}📦`;
+        } else {
+            return `${profit.toString().padEnd(3)}📦`;
+        }
+    } else {
+        const profit = item2_amount * item2_value - item1_amount * item1_value;
+
+        if (profit === 0) {
+            return "---";
+        } else if (profit > 0) {
+            return `+${profit.toString().padEnd(2)}💎`;
+        } else {
+            return `${profit.toString().padEnd(3)}💎`;
+        }
+    }
+}
+
+$( document ).ready(() => {
+    $( "#dailyDeals th:last-child" ).before($( "<th>" ).text("Profit"));
+    $( "#dailyDeals td:last-child" ).before($( "<td>", {
+        class: "vertical-middle",
+        "data-bind": "text: get_profit()",
+    }));
+});
+
 keymage('underground', 'shift-1', () => { select_tool("Chisel"); }, { preventDefault: true });
 keymage('underground', 'shift-2', () => { select_tool("Hammer"); }, { preventDefault: true });
 
